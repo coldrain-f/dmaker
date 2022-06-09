645a105188468f07587f5300107deb35ecc4b3a3
coldrain-f <intmain__@naver.com>
2022-06-09 16:01:30 +0900

    Initial commit

---
+++ src/test/java/me/coldrain/dmaker/DmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.coldrain.dmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
